9dc2530ae3a040e54a76d80c16d4d59829fcccfd
kristenmills <kristen@kristen-mills.com>
2014-01-29 21:46:01 -0500

    Added some helper methods

--- mycroft/client.py
+++ mycroft/client.py
@@ -1,4 +1,4 @@
-import asyncore, socket
+import asyncore, socket, re, json
 
 class MycroftClient(asynccore.dispatcher):
 
@@ -6,6 +6,7 @@ class MycroftClient(asynccore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
+        self.dependencies = {}
 
     def handle_connect(self):
         pass
@@ -15,3 +16,37 @@ class MycroftClient(asynccore.dispatcher):
 
     def handle_close(self):
         pass
+
+    # Parses a message
+    def parse_message(self, msg):
+        regex = re.compile('([A-Z_]*) ({.*})$')
+        match = regex.match(msg)
+        if match is None:
+            regex = re.compile('([A-Z_]*)')
+            match = regex.match(msg)
+            if match is None:
+                raise Exception('Error: Malformed Message')
+            msg_type = match.groups()[0]
+            data = {}
+        else:
+            msg_type = match.groups()[0]
+            data = json.loads(match.groups()[1])
+        return {'type': msg_type, 'data': data}
+
+    # Sends a message of a specific type
+    def send_message(self, msg_type, message=None):
+        message = '' if message is None else json.dumps(message)
+        body = type + ' ' + message
+        body = body.strip()
+        length = len(body)
+        print('Sending Message')
+        print(str(length))
+        print(body)
+        self.send("{0}\n{1}".format(length, body))
+
+    # Updates dependencies
+    def update_dependencies(self, deps):
+        for capability, instance in deps.iteritems():
+            self.dependencies[capability] = self.dependencies[capability] or {}
+            for appId, status in instance.iteritems():
+                self.dependencies[capability][appId] = status
