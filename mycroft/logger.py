73807976953d866fe92b2ac97d055edb885d84a2
Robert McLaughlin <robert@sparkk.us>
2014-03-03 23:05:25 -0500

    add documentation to logger

--- mycroft/logger.py
+++ mycroft/logger.py
@@ -1,8 +1,9 @@
 import logging
-BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 
 # The background is set with 40 plus the number of the color,
 # and the foreground with 30
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
 
 #These are the sequences need to get colored ouput
 RESET_SEQ = "\033[0m"
@@ -27,12 +28,24 @@ COLORS = {
 
 
 class ColoredFormatter(logging.Formatter):
+    """
+    Logging formatter for logging colored messages
+    """
 
     def __init__(self, msg, use_color=True):
-        logging.Formatter.__init__(self, msg)
+        """
+        Create a new formatter
+        Args:
+            msg - the format string
+            use_color - True to use color in the terminal
+        """
+        super().__init__(msg)
         self.use_color = use_color
 
     def format(self, record):
+        """
+        Format the given logging record
+        """
         levelname = record.levelname
         if self.use_color and levelname in COLORS:
             levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
